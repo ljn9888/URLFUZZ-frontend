64ca9d755d6ed18140934a4ef86ec6bb54d51c7a
ljn9888 <ljn9888@yeah.net>
2022-04-03 02:58:06 -0400

    good version

--- ctrl/index.js
+++ ctrl/index.js
@@ -1,11 +1,67 @@
+
 (function () {
-    angular.module('app', ['navservice'])
+    angular.module('app', ['navservice', 'httpservice'])
         .controller('index_ctrl', IndexController);
     
-    function IndexController($scope, $window, NavHeaderService){
+    function IndexController($scope, $http, $interval, NavHeaderService){
+        $scope.inputurl = "";
+        $scope.search = "";
         NavHeaderService.navheader_init(true);
-        $scope.to_aptlist = function(){
-            $window.location.href = "/page/apt-list.html";
+ 
+
+//////////////////////////////
+        // var c=0;
+        // $scope.message="This DIV is refreshed "+c+" time.";
+        // $interval(function() {
+        //         $scope.message="This DIV is refreshed "+c+" time.";
+        //         c++;
+        // },1000);
+        var result = document.getElementById("result");
+        
+        /////////////////
+        $scope.to_backend = function(){
+            alert('d');
+            // $http.post('http://0.0.0.0:8082', inputurl);
+            $interval(function() {
+                $http({method :'GET', url: 'http://0.0.0.0:8082'}).
+                then(function(response) {
+                    if(response.data != $scope.search && response.data != 'Request failed') {
+                        $scope.search = response.data ;
+                        var tag = document.createElement("tr");
+                        var text = document.createTextNode($scope.search);
+                        tag.appendChild(text);
+                        result.appendChild(tag);
+                    }
+              });
+              }, 5);
+
+
+
+
+
+            };
+            // function(){
+            
+            // $scope.search  =  $http.get('http://0.0.0.0:8082/');
+            // };
         }
     }
-})();
+)();
+
+
+
+// (function () {
+//     angular.module('app', ['navservice', 'httpservice'])
+//         .controller('index_ctrl', IndexController);
+    
+//     function IndexController($scope, $http, NavHeaderService){
+//         $scope.inputurl = "";
+//         $scope.search;
+//         NavHeaderService.navheader_init(true);
+//         $scope.to_backend = function(){
+//             search = function(){
+//                 return $http.get(url);
+//             };
+//         }
+//     }
+// })();
